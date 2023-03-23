1dab97ff2b8c8fd3a2e78017e84379e5bd58c45f
privac <me@byeonlab.org>
2023-03-23 14:25:20 +0900

    api test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require("cheerio");
+const inquirer = require("inquirer");
 
 const headers = {
     "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"
@@ -13,9 +14,9 @@ const urlPrefix = "https://gall.dcinside.com/board/lists?id=";
 const galleryID = "programming";
 const galleryPage = "1";
 
-const getPostList = async () => {
+const getPageHtml = async (url) => {
     try {
-      return await axios.get(buildTargetUrl(urlPrefix, galleryID, galleryPage), {
+      return await axios.get(url, {
         headers: headers
       });
     } catch (error) {
@@ -23,27 +24,59 @@ const getPostList = async () => {
     }
 };
 
-getPostList()
+getPageHtml(buildTargetUrl(urlPrefix, galleryID, galleryPage))
     .then(html => {
         let posts = []
         const $ = cheerio.load(html.data);
 
         const $trs = $("table.gall_list tr.us-post")
         $trs.each(function (i, e) {
-                posts[i] = {
-                    no: $(this).find("td.gall_num").text(),
-                    title: $(this).find("td.gall_tit a").text(),
-                    uri: $(this).find("td.gall_tit a").attr("href"),
-                    // 고닉: 닉네임(O), IP(X), UID(O)
-                    // 유독: 닉네임(O), IP(O), UID(X)
-                    writerNick: $(this).find("td.gall_writer.ub-writer").attr("data-nick"),
-                    writerIP: $(this).find("td.gall_writer.ub-writer").attr("data-ip"),
-                    writerUID: $(this).find("td.gall_writer.ub-writer").attr("data-uid"),
-                    date: $(this).find("td.gall_date").attr("title")
+                no = $(this).find("td.gall_num").text(),
+                title = $(this).find("td.gall_tit a").text(),
+                url = "https://gall.dcinside.com" + $(this).find("td.gall_tit a").attr("href"),
+                writerNick = $(this).find("td.gall_writer.ub-writer").attr("data-nick"),
+                writerIP = $(this).find("td.gall_writer.ub-writer").attr("data-ip"),
+                writerUID = $(this).find("td.gall_writer.ub-writer").attr("data-uid"),
+                date = $(this).find("td.gall_date").attr("title"),
 
+                posts[i] = {
+                    name: no + " | " +  writerNick + " | "  + title, 
+                    value: {
+                      no: no,
+                      title: title,
+                      url: url,
+                      // 고닉: 닉네임(O), IP(X), UID(O)
+                      // 유독: 닉네임(O), IP(O), UID(X)
+                      writerNick: writerNick,
+                      writerIP: writerIP,
+                      writerUID: writerUID,
+                      date: date
+                    }
                 }
             }
         )
         return posts;
     }
-).then(res => console.log(res));
+).then(posts => {
+     inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'post',
+        message: 'gallery',
+        choices: posts,
+      },
+    ]).then(answers => {
+      getPageHtml(answers.post.url)
+      .then(html => {
+        const $ = cheerio.load(html.data);
+        const $ps = $("div.write_div p");
+
+        $ps.each(function (index, element) {
+          console.log($(this).text());
+        });
+      });
+    }
+    )
+  }
+);
